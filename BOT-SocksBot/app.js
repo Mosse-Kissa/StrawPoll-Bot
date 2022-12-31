fbbee7fb18be01127305949c67e7f99585900bde
Mosse-Kissa <121736123+Mosse-Kissa@users.noreply.github.com>
2023-01-01 00:42:22 +0200

    Update app.js

--- BOT-SocksBot/app.js
+++ BOT-SocksBot/app.js
@@ -4,4 +4,4 @@ console.log("Iniciando bot :)");
 
 
 // Args: (url, optionID, protection, socksFilePath, amount)
-botPoll('https://www.strawpoll.me/17311284', 1, true, join(__dirname, 'socks.txt'));
+botPoll('https://strawpoll.com/polls/2ayLWx5zMZ4', 1, true, join(__dirname, 'socks.txt'));
